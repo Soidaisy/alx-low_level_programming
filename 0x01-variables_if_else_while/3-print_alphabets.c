706c55083f2620a6929bb7399feeeb1121c0e719
Daisy Soi <soidchebet@gmail.com>
2023-04-24 13:05:23 -0700

    3. alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
+
 /**
-*main - Prints the alphabet.
-*/
-Return: Always 0(Success)
+ * main - prints the alphabet in lowercase, and then in uppercase,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-char alp(52)="abcdefghijklmnopqrsyuvwxyz",
-int i;
-for (i=0,i<52,i++)
-{
-putchar(alp[i]);
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
 }
-putchar ("n");
-return 0
+
+

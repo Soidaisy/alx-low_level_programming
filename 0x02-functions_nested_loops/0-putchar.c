e1a34184d6f5dba5faf8f4e37c72cbb7b1dc023a
Daisy Soi <soidchebet@gmail.com>
2023-04-25 12:16:07 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,20 @@
-#include<unistd.h>
+#include "main.h"
 /**
- * *_putchar -writes the character c stdout
- * *@c: The character to print
-*Return:On success 1.
-*On error,-1 is returned,and errno is set appropriately.
-*/
-int _putchar(char c)
+ * main -Entry point
+ * Description:"Prints all the numbers of base 16 in lowercase"
+ * Return: Always 0(Success)
+	 */
+int main(void)
 {
-	  Return (write(1,&c,1));
-}
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+	}
+

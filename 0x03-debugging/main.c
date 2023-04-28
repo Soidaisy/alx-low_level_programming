e3cfe0370ce166d2884356e83eb9ce333886f597
Daisy Soi <soidchebet@gmail.com>
2023-04-28 12:02:55 -0700

    2-largest_number.c

--- 0x03-debugging/main.c
+++ 0x03-debugging/main.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
-* main - tests function that prints if integer is positive or negative
+*positive _or_negative function that prints if integer is positive or negative
+*@i:number to work on
 * Return: 0
 */
-
-int main(void)
+void positive_or_negative(int i)
 {
-        int i;
-
-        i = 98;
-        positive_or_negative(i);
-
-        return (0);
+if (i>0)
+printf("%d is positive\n",i)
+else if (i<0)
+printf("%d is negative\n",i);
+else
+printf("%d is zero\n",i)
 }

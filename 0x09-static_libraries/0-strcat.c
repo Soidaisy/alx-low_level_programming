35b5fadb930b683eddf94a6a1c2a2d5aa9805ef6
Daisy Soi <soidchebet@gmail.com>
2023-05-14 13:37:54 -0700

    Task 0

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,32 +1,10 @@
 #include "main.h"
-/**
-* _strncat - concatenate two strings
-* using at most n bytes from src
-* @dest: input value
-* @src: input value
-* @n: input value
-*
-* Return: dest
-*/
-char *_strncat(char *dest, char *src, int n)
-{
-int i;
-int j;
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
-
-i = 0;
-while (dest[i] != '\0')
+char *_strcat(char *dest, char *src)
 {
-i++;
+    return strcat(dest, src);
 }
-j = 0;
-while (j < n && src[j] != '\0')
-{
-dest[i] = src[j];
-i++;
-j++;
-}
-dest[i] = '\0';
-return (dest);
-}
-
